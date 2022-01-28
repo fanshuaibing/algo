81565404fc8ed44a832f0f507a245da0c162bd86
fan <fatfan1020@gmail.com>
2022-01-28 15:58:21 +0800

    update

---
+++ leetcode/linkedList/142.js
@@ -0,0 +1,18 @@
+var detectCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      slow = head;
+
+      while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+      return fast;
+    }
+  }
+  return null;
+};
